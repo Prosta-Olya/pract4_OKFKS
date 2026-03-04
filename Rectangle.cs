3d4996771af3356b55eb6115c9a3da59a4dd4e0f
aa <aa>
2026-03-04 15:32:50 +0500

    создан проект

---
+++ Rectangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract4_OKFKS
+{
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            string str = Validation(width, height);
+            if (str == "")
+            {
+                Width = width;
+                Height = height;
+            }
+        }
+
+        public string Validation(double width, double height)
+        {
+            string str = "";
+            if (width <= 0 || height <= 0)
+            {
+                str = "Стороны прямоугольника не могут быть меньше 0 или равны ему";
+            }
+            return str;
+        }
+
+        public override double Area()
+        {
+            return Width * Height;
+        }
+
+        public override double Perimeter()
+        {
+            return Width + Width + Height + Height;
+        }
+
+        public override string ToString()
+        {
+            return $"Ширина прямоугольника = {Width} \nВысота прямоугольника = {Height}";
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract4_OKFKS
+{
+    public class Triangle : Shape
+    {
+        private double SideA { get; set; }
+        private double SideB { get; set; }
+        private double SideC { get; set; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            string str = Validation(sideA, SideB, sideC);
+            if(str == "")
+            {
+                SideA = sideA;
+                SideB = sideB;
+                SideC = sideC;
+            }
+        }
+
+        public string Validation(double sideA, double sideB, double sideC)
+        {
+            string str = "";
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+            {
+                str = "Стороны треугольника не могут быть меньше 0 или равны ему";
+            }
+            if (sideA > sideB + sideC)
+            {
+                str = "Сторона А имеет некорректное значение";
+            }
+            if (sideB > sideA + sideC)
+            {
+                str = "Сторона B имеет некорректное значение";
+            }
+            if (sideC > sideB + sideA)
+            {
+                str = "Сторона C имеет некорректное значение";
+            }
+            return str;
+        }
+
+        public override double Area()
+        {
+            double p = Perimeter() / 2;
+            return Math.Sqrt((p * (p - SideA) * (p - SideB) * (p - SideC)));
+        }
+
+        public override double Perimeter()
+        {
+            return SideA + SideB + SideC;
+        }
+
+        public override string ToString()
+        {
+            return $"Стороны треугольника: A = {SideA}, B = {SideB}, C = {SideC}";
+        }
+    }
+}
